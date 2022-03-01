0320fb179575427b14558844d7323714262d53ca
Kacper Siuda <43751280+ksiuda93@users.noreply.github.com>
2022-03-01 21:04:43 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the Home page")
+	fmt.Fprint(w, "Welcome to the Home page v2")
 	fmt.Println("Endpoint hit: homePage ")
 }
 
